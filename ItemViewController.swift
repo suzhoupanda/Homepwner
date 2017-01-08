840fd03b594acc5f968588aca8d1ac3311626987
yongrenwang <czie.alexmak@gmail.com>
2017-01-08 18:49:44 +0800

    Set the contentInsets and scrollIndicatorInsets of the tableView to that the top edge inset of the scroll indicator and the tableView content are set equal to the height of the status bar; this way, tableView does not underlap the status bar

--- ItemViewController.swift
+++ ItemViewController.swift
@@ -15,6 +15,13 @@ class ItemsViewController: UITableViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+        
     }
     
     override func didReceiveMemoryWarning() {
