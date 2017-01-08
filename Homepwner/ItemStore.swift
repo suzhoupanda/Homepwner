08b99992c60f1f1c5a1d20bb9b0c321a3c2a06a1
yongrenwang <czie.alexmak@gmail.com>
2017-01-08 17:10:18 +0800

    Created classes for data model(i.e. Items class,which inherits from NSObject and which has both a designated and a convenience initializer; and the ItemStore class); the convenience initializer for Items uses the arc4random_uniform function to assign a random name, random serialNumber, and random dollar value to the corresponding stored properties for item

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,13 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Aleksander Makedonski on 1/8/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore{
+    var allItems = [Item]()
+}
---
+++ ItemViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ItemViewController.swift
+//  Homepwner
+//
+//  Created by Aleksander Makedonski on 1/8/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
