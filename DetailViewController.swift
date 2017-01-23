4c18e9f1c58d045f49fcddba9754a90ad27b2e55
yongrenwang <czie.alexmak@gmail.com>
2017-01-23 13:29:22 +0800

    Added IBOutlets for dateLabel and textFields

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -12,4 +12,12 @@ import UIKit
 
 class DetailViewController: UIViewController{
     
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var serialNumberField: UITextField!
+    
+    @IBOutlet weak var valueField: UITextField!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
 }
