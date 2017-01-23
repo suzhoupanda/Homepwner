1dd76b354c82b23e761fc279b604182da7c447fc
yongrenwang <czie.alexmak@gmail.com>
2017-01-23 13:10:24 +0800

    Added a new detailviewcontroller.swift file

---
+++ DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Aleksander Makedonski on 1/23/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class DetailViewController: UIViewController{
+    
+}
