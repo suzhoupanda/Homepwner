bc275a554bd4a70a6e6693fd2d9606db653f3db0
yongrenwang <czie.alexmak@gmail.com>
2017-01-23 14:52:34 +0800

    In AppDelegate file, set the navigation controller as rootViewController

--- Homepwner/AppDelegate.swift
+++ Homepwner/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         let itemStore = ItemStore()
         
-        let itemsController = window!.rootViewController as! ItemsViewController
-        itemsController.itemStore = itemStore
+        let navController = window!.rootViewController as! UINavigationController
+        let itemsController = navController.topViewController as! ItemsViewController
         
         return true
     }
